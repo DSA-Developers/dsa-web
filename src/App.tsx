34dd9169dd7be35494c81a296f1fa714e6a66829
jugnugs <jnguyen3@ufl.edu>
2021-03-04 16:06:39 -0500

    clean up & set up proper ts tests

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import './App.css';
 import { TextField } from './components/TextField';
 
-const App = (): JSX.Element => {
-    return (
-        <div className="App">
-            <TextField text="hi" />
-        </div>
-    );
+const App: React.FC = () => {
+  const [title, setTitle] = React.useState(document.title);
+  return (
+    <div className="App">
+      <p>{title === '' ? 'no title :(' : title}</p>
+      <TextField
+        text={title}
+        handleChange={(e) => {
+          const newTitle = e.target.value;
+          document.title = newTitle;
+          setTitle(newTitle);
+        }}
+      />
+    </div>
+  );
 };
 
 export default App;
--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 interface Props {
-    text: string;
+  text: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const TextField: React.FC<Props> = () => {
-    return (
-        <div>
-            <input />
-        </div>
-    );
-};
+export const TextField: React.FC<Props> = ({ text, handleChange }) => {
+  return (
+    <div>
+      <input value={text} onChange={handleChange} />
+    </div>
+  );
+}
