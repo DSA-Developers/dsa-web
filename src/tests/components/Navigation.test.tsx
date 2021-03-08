9d90eda9c38458cf1f40a7079e24d99351367c94
jugnugs <jnguyen3@ufl.edu>
2021-03-08 14:16:25 -0500

    add enzyme & create nav tests

--- src/tests/components/Navigation.test.tsx
+++ src/tests/components/Navigation.test.tsx
@@ -1,14 +1,33 @@
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { shallow } from 'enzyme';
+import Nav from 'react-bootstrap/Nav';
 import { Navigation } from '../../components/Navigation';
 
-test('Navigation bar renders', () => {
-  const props = {};
-  const component = renderer.create(
-    <Router>
-      <Navigation {...props} />
-    </Router>,
-  );
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Navigation', () => {
+  const wrapper = shallow(<Navigation />);
+  const links = wrapper.find(Nav.Link);
+
+  test('Nav bar renders 4 links', () => {
+    expect(links).toHaveLength(4);
+  });
+
+  test('Nav bar renders the proper link names', () => {
+    expect(links.at(0).text()).toEqual('Home');
+    expect(links.at(1).text()).toEqual('About');
+    expect(links.at(2).text()).toEqual('Gallery');
+    expect(links.at(3).text()).toEqual('Contact');
+  });
+
+  // snapshot test
+  test('Navigation bar renders', () => {
+    const props = {};
+    const component = renderer.create(
+      <Router>
+        <Navigation {...props} />
+      </Router>,
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
