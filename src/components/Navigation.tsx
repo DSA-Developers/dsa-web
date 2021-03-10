1b0723fa709aae096eede342c4401df6b49591ed
jugnugs <jnguyen3@ufl.edu>
2021-03-10 11:26:24 -0500

    style navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,19 +7,27 @@ interface Props {}
 
 export const Navigation: React.FC<Props> = ({}) => {
   return (
-    <Navbar className="justify-content-end">
-      <Nav>
+    <Navbar bg="light" variant="light" className="navbar border-bottom">
+      <Nav className="ml-auto">
         <Nav.Item>
-          <Nav.Link as={Link} to="/home">Home</Nav.Link>
+          <Nav.Link as={Link} to="/home">
+            Home
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/about">
+            About
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
+          <Nav.Link as={Link} to="/gallery">
+            Gallery
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          <Nav.Link as={Link} to="/contact">
+            Contact
+          </Nav.Link>
         </Nav.Item>
       </Nav>
     </Navbar>
